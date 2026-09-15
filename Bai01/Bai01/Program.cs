fe67c6c42feaa2cfb33230ee8d2e344e297b322f
Ging Pham <thanhhoa13052006@gmail.com>
2026-09-15 17:45:04 +0700

    Tuan01/Bai01/In nhan

---
+++ Bai01/Bai01/Program.cs
@@ -0,0 +1,18 @@
+﻿//In nhãn
+
+using System;
+
+namespace Bai01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("********************************");
+            Console.WriteLine("* Truong: Dai Hoc HUFLIT       *");
+            Console.WriteLine("* Khoa: CNTT                   *");
+            Console.WriteLine("* Ho ten:                      *");
+            Console.WriteLine("********************************");
+        }
+    }
+}
