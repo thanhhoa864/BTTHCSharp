65c307af422d53b41f01fdb176e8cfefe87cd821
Ging Pham <thanhhoa13052006@gmail.com>
2026-09-15 17:48:14 +0700

    Tuan01/Bai03/Doi sang giay

---
+++ Bai03/Bai03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Đổi sang giây
+
+using System;
+
+namespace Bai03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập số giờ: ");
+            int hours = int.Parse(Console.ReadLine()); // nhập số giờ
+            Console.WriteLine("Nhập số phút: ");
+            int minutes = int.Parse(Console.ReadLine()); // nhập số phút
+            Console.WriteLine("Nhập số giây: ");
+            int seconds = int.Parse(Console.ReadLine()); // nhập số giây
+            Console.WriteLine($"Số giây = {hours * 3600 + minutes * 60 + seconds}");// in ra số giây
+        }
+    }
+}
