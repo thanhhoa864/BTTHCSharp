900629a0a8b5a1ea5bb9f6c40a90dbad676831fe
Ging Pham <thanhhoa13052006@gmail.com>
2026-09-15 17:50:33 +0700

    Tuan01/Bai06/Ham mu

---
+++ Bai06/Bai06/Program.cs
@@ -0,0 +1,18 @@
+﻿// nhập số thực a và n tính a mũ n
+
+using System;
+namespace Bai06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập số thực a: ");
+            double a = double.Parse(Console.ReadLine()); // Nhập số thực a từ bàn phím
+            Console.Write("Nhập số nguyên n: ");
+            int n = int.Parse(Console.ReadLine()); // Nhập số nguyên n từ bàn phím
+            double result = Math.Pow(a, n); // Tính a mũ n sử dụng phương thức Math.Pow
+            Console.WriteLine($"{a} mũ {n} = {result}"); // In ra kết quả
+        }
+    }
+}
