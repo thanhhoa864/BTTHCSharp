ac0c9b92b262aeca8150766f392e018b8d598a1c
Ging Pham <thanhhoa13052006@gmail.com>
2026-09-15 17:50:09 +0700

    Tuan01/Bai05/Tinh dien tich tam giac

---
+++ Bai05/Bai05/Program.cs
@@ -0,0 +1,18 @@
+﻿// tính diện tích tam giác theo công thức Heron
+
+using System;
+namespace Bai05 { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập độ dài 3 cạnh của tam giác:"); //Nhập độ dài 3 cạnh của tam giác
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double p = (a + b + c) / 2; // nửa chu vi
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c)); // công thức Heron
+            Console.WriteLine($"Diện tích tam giác là: {area}");
+        }
+    }
+}
