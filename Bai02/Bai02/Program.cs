6005b0807c0a5146a6e9040787fe07205361f414
Ging Pham <thanhhoa13052006@gmail.com>
2026-09-15 17:46:05 +0700

    Tuan01/Bai02/Tinh dien tich va chu vi hinh tron

---
+++ Bai02/Bai02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Tính diện tích và chu vi hình tròn
+
+using System;
+
+namespace Bai02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập bán kính hình tròn: ");
+            double radius = Convert.ToDouble(Console.ReadLine()); // Nhập bán kính hình tròn từ người dùng
+            double area = Math.PI * Math.Pow(radius, 2); // Tính diện tích hình tròn
+            double circumference = 2 * Math.PI * radius; // Tính chu vi hình tròn
+            Console.WriteLine($"Diện tích hình tròn: {area}"); // In ra diện tích hình tròn
+            Console.WriteLine($"Chu vi hình tròn: {circumference}");// In ra chu vi hình tròn
+        }
+    }
+}
