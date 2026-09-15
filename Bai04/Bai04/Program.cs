d8b1b49efeeebb27bf725b61f39d76ed2b17ed05
Ging Pham <thanhhoa13052006@gmail.com>
2026-09-15 17:49:45 +0700

    Tuan01/Bai04/Doi sang gio phut giay

---
+++ Bai04/Bai04/Program.cs
@@ -0,0 +1,19 @@
+﻿//chuyển giây sang giờ, phút, giây
+
+using System;
+
+namespace Bai04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập số giây: ");
+            int totalSeconds = int.Parse(Console.ReadLine()); // Nhập số giây từ người dùng
+            int hours = totalSeconds / 3600; // Tính số giờ
+            int minutes = (totalSeconds % 3600) / 60; // Tính số phút
+            int seconds = totalSeconds % 60; // Tính số giây còn lại
+            Console.WriteLine($"{totalSeconds} giây = {hours} giờ, {minutes} phút, {seconds} giây"); // In ra kết quả
+        }
+    }
+}
