914b7c1c80aa3890fc658c643a4678199fe31d07
Ging Pham <thanhhoa13052006@gmail.com>
2026-09-15 17:47:50 +0700

    Tuan01/Bai02NC/Do sang radian

---
+++ Bai02NangCao/Bai02NangCao/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//chuyển độ góc sang radian
+using System;
+
+namespace Bai02NangCao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập độ bạn muốn chuyển đổi: ");
+            double doGoc = double.Parse(Console.ReadLine());
+
+            // Công thức chuyển độ sang radian: radian = độ * (Pi / 180)
+            double radian = doGoc * Math.PI / 180;
+
+            Console.WriteLine($"{doGoc} độ tương đương với {radian:F4} radian.");
+        }
+    }
+}
